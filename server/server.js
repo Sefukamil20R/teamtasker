9f0def2353447ed81d27bb8111a124bcff3971c8
Sefukamil20R <Tenzilk20@gmail.com>
2025-05-03 15:40:16 +0300

    CORS issue fixed

--- server/server.js
+++ server/server.js
@@ -17,6 +17,8 @@ const allowedOrigins = [
     'http://localhost:5173',
     'http://localhost:3008',
     'https://teamtasker.netlify.app',
+    'https://team-tasker.netlify.app',
+
 ];
 
 const corsOptions = {
